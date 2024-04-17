020899cf12e99c1044d99d0954f5c40e6ab06d04
Brooke Anderson <brookeashleyanderson88@gmail.com>
2024-04-16 23:47:33 -0600

    first commit

---
+++ data/hotels.js
@@ -0,0 +1,51 @@
+export const hotels = [
+    {
+        name: "Ritz Plaza",
+        hotelImg: "ritz-1.webp",
+        address: ["1234 west","Los Angeles","California"],
+        tel: "+1 (555) 890-1234"
+    },
+    {
+        name: "Ritz Azure cove",
+        hotelImg: "ritz-2.webp",
+        address: ["123 Sapphire Street","Azure Cove","Muggle World"],
+        tel: "+1 (555) 789-0123"
+    },
+    {
+        name: "Ritz Grand Highlands",
+        hotelImg: "ritz-3.webp",
+        address: ["456 Highland Avenue","Highland","Oxford"],
+        tel: "+1 (555) 678-9012"
+    },
+    {
+        name: "Ritz Emerald Bay",
+        hotelImg: "ritz-4.webp",
+        address: ["789 Emerald Lane","Serenity Valley","Ontario"],
+        tel: "+1 (555) 567-8901"
+    },
+    {
+        name: "Ritz Sunset Coast",
+        hotelImg: "ritz-5.webp",
+        address: ["285 Beach Expressway","Huntington","Washington"],
+        tel: "+1 (555) 456-7890"
+    },
+    {
+        name: "Ritz Crystal Cove",
+        hotelImg: "ritz-6.webp",
+        address: ["890 Cove Pkwy","Sunset Ridge","Texas"],
+        tel: "+1 (555) 345-6789"
+    },
+    {
+        name: "Ritz Royal Gardens",
+        hotelImg: "ritz-7.webp",
+        address: ["234 Golden Road","Royal Gardens","Exforia"],
+        tel: "+1 (555) 234-5678"
+    },
+    {
+        name: "Ritz Harbor",
+        hotelImg: "ritz-8.webp",
+        address: ["1791 Sandy Way","Rivertown","Harborn"],
+        tel: "+1 (555) 123-4567"
+    },
+]
+
---
+++ js/scripts.js
@@ -0,0 +1,64 @@
+import {hotels} from '../data/hotels.js';
+
+const hb = document.querySelector('#hamburgerButton');
+const cb = document.querySelector('#closeButton');
+const pw = document.querySelector('#pageWrapper');
+
+hb.addEventListener('click', () => {
+pw.classList.toggle('moveOver');
+hb.classList.toggle('open')
+cb.classList.toggle('open')
+});
+
+cb.addEventListener('click', () => {
+pw.classList.toggle('moveOver');
+hb.classList.toggle('open')
+cb.classList.toggle('open')
+});
+
+//inject current year in footer
+const variableOne = new Date();
+document.querySelector('#year').textContent = variableOne.getFullYear();
+
+//grab tht hotel location data
+
+console.log(hotels);
+const cards=document.querySelector('#properties')
+
+hotels.forEach(building => {
+  const mySection = document.createElement('section')
+
+const theImage = document.createElement("img")
+theImage.src = `images/${building.hotelImg}`
+theImage.alt = building.name
+
+const theName = document.createElement("h2")
+theName.textContent = building.name
+
+const theAddress = document.createElement("p");
+theAddress.innerHTML = `${building.address[0]}<br>${building.address[1]}, ${building.address[2]}`;
+
+const theTelephone = document.createElement("a");
+theTelephone.href = `tel:${building.tel}`
+theTelephone.textContent = building.tel
+
+mySection.appendChild(theImage)
+mySection.appendChild(theName)
+mySection.appendChild(theAddress)
+mySection.appendChild(theTelephone)
+cards.appendChild(mySection)
+})
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    effect:'slide',
+    speed:1500,
+    autoplay: {
+        delay: 3000,
+    },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
